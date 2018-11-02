15cb411b382cd2ce8071635e905c11976eaa87de
Jaeyu Jo <ehay@naver.com>
2018-11-02 17:10:49 +0900

    First Cycle Add 1
    add:practice030

---
+++ first_cycle/src/practice030/BioCalendar.java
@@ -0,0 +1,25 @@
+package practice030;
+
+/**
+ * Created by ehay@naver.com on 2018-11-01
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class BioCalendar {
+
+    public static final int PHYSICAL = 23;
+
+    public double getBioRhythm(long days, int index, int max) {
+        return max - Math.sin((days % index) * 2 * Math.PI / index);
+    }
+
+    public static void main(String[] args) {
+        int days = 1200;
+
+        BioCalendar bio = new BioCalendar();
+
+        double phyval = bio.getBioRhythm(days, PHYSICAL, 100);
+        System.out.printf("나의 신체 지수 %1$.2f입니다.\n", phyval);
+    }
+}
